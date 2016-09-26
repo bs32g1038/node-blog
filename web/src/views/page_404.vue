f26ba433ac38ef9f2ffe9db662d750b246c79d65
bs32g1038 <bs32g1038@163.com>
2016-09-26 09:51:54 +0800

    add page_404

---
+++ web/src/views/page_404.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <PathNav :paths="paths"></PathNav>
+        <section class="error">
+            <div class="error-message">
+                <div class="error-type">404</div>
+                <div class="error-issue">抱歉！您访问的页面不存在......</div>
+                <router-link :to="'/'" class="error-button"><i class="fa fa-fw fa-home"></i>回首页</router-link>
+                <a  class="error-button" href="#" onClick="javascript:history.back(-2);"><i class="fa fa-fw fa-reply"></i>上一步</a>            
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+    import PathNav from '../components/PathNav.vue'
+
+    export default {
+        components: {
+            PathNav
+        },
+        data() {
+            return {
+                paths: [{
+                    url: '',
+                    name: '错误404页面'
+                }]
+            }
+        },
+        beforeMount() {
+            this.fetchData(this.$store)
+        },
+        methods: {
+            fetchData(store) {
+                return store.dispatch('loadInitData');
+            }
+        },
+        preFetch: function(store) {
+            return this.methods.fetchData(store);
+        }
+    }
+</script>
