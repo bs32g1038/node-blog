cdd9e5df94ce05d9b16b01ebc40bb3fe25d695e5
bs32g1038 <bs32g1038@163.com>
2018-03-10 14:20:26 +0800

    repair upload store file

--- server/middlewares/StoreFile.js
+++ server/middlewares/StoreFile.js
@@ -1,25 +1,20 @@
-/*
- * @Author: bs32g1038@163.com 
- * @Date: 2017-05-18 17:20:33 
- * @Last Modified by: bs32g1038@163.com
- * @Last Modified time: 2017-07-01 22:40:53
- */
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const moment = require('moment');
 const config = require('../config');
-let storage; //记录存储方式
+const utils = require('utility');
+let storage;
 storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        const basePath = 'static/images/' + moment().format("YYYY");
+        const basePath = 'static/upload/' + moment().format("YYYY");
         if (!fs.existsSync(basePath)) {
             fs.mkdirSync(basePath);
         }
         cb(null, basePath);
     },
     filename: function(req, file, cb) {
-        cb(null, moment().format("YYYY") + path.extname(file.originalname));
+        cb(null, utils.md5(utils.YYYYMMDDHHmmss() + file.originalname) + path.extname(file.originalname));
     }
 });
 let upload = multer({
@@ -32,7 +27,7 @@ let uploadLocal = function(req, res, next) {
             return next(err);
         }
         try {
-            let url = '/static/images/' + moment().format("YYYY") + '/' + req.file.filename;
+            let url = '/static/upload/' + moment().format("YYYY") + '/' + req.file.filename;
             return res.json({ url: url });
         } catch (error) {
             return next(error);
