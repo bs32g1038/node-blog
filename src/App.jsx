43af2e1575dc463836955558ca482e4b4893f79a
bs32g1038 <bs32g1038@163.com>
2018-03-18 15:42:45 +0800

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Articles from './components/Articles';
 import Guestbooks from './components/Guestbooks';
 import Article from './components/Article';
 import About from './components/About';
-import Music from './components/Music';
+import Music from './components/Music/index';
 import axios from './utils/axios'; 
 
 export default class App extends React.Component {
