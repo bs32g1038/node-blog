581998bd0e3833721dfa66a4324573cd676e219b
bs32g1038 <bs32g1038@163.com>
2018-07-21 14:37:24 +0800

    update link css

--- react-blog/src/components/friendly-links/index.jsx
+++ react-blog/src/components/friendly-links/index.jsx
@@ -28,7 +28,7 @@ export default class FriendlyLinks extends Component {
                         <h2 className="FriendlyLinks-headerTitle">--友情链接--</h2>
                         <p>Web海洋的小船，不一样的友谊🐎</p>
                     </header>
-                    <h3 className="FriendlyLinks-tip" style={{ textAlign: 'center', backgroundColor: '#efefef' }}><i className="fa fa-hand-o-down fa-fw"></i>区域</h3>
+                    {/* <h3 className="FriendlyLinks-tip" style={{ textAlign: 'center', backgroundColor: '#efefef' }}><i className="fa fa-hand-o-down fa-fw"></i>区域</h3> */}
                     <ul className="FriendlyLinks-body">
                         {
                             this.state.links.map((item) => (
@@ -39,6 +39,7 @@ export default class FriendlyLinks extends Component {
                                                 <img className="FriendlyLinks-itemImg" src={item.logo} />{item.name}
                                             </div>
                                             <p className="FriendlyLinks-itemDescription">{item.description}</p>
+                                            <i className="fa fa-link FriendlyLinks-itemDecoration"></i>
                                         </a>
                                     </div>
                                 </li>
@@ -46,13 +47,15 @@ export default class FriendlyLinks extends Component {
                         }
                     </ul>
                     <footer className="FriendlyLinks-footer">
-                        <h3 className="FriendlyLinks-tip"><i className="fa fa-info-circle fa-fw"></i>链接须知</h3>
+                        <hr className="FriendlyLinks-hr"/>
+                        <h3 className="FriendlyLinks-tip"><i className="fa fa-info-circle fa-fw"></i>友情链接添加须知</h3>
                         <ol>
                             <li className="FriendlyLinks-li">请确定贵站可以稳定运营</li>
                             <li className="FriendlyLinks-li">原创博客优先，技术类博客优先，设计、视觉类博客优先</li>
                             <li className="FriendlyLinks-li">经常过来访问和评论，眼熟的</li>
                             <li className="FriendlyLinks-li">请避免广告链接</li>
                         </ol>
+                        <hr className="FriendlyLinks-hr"/>
                         <h3 className="FriendlyLinks-tip"><i className="fa fa-map fa-fw"></i>友链申请规范</h3>
                         <ol>
                             <li className="FriendlyLinks-li"><strong>名称：</strong>站点名称</li>
@@ -60,8 +63,11 @@ export default class FriendlyLinks extends Component {
                             <li className="FriendlyLinks-li"><strong>描述：</strong>80字以内站点描述</li>
                             <li className="FriendlyLinks-li"><strong>图标：</strong>请自行提供logo（格式为.png，.jpg）图片地址</li>
                         </ol>
+                        <hr className="FriendlyLinks-hr"/>
                         <h3 className="FriendlyLinks-tip"><i className="fa fa-hand-o-right fa-fw"></i>友链申请操作</h3>
-                        <p>要申请友链的小伙伴们，请到博客<Link to="/blog/guestbook" className="FriendlyLinks-jump">留言版</Link>进行留言，我将会在收到的第一时间进行添加，谢谢！</p>
+                        <ol>
+                            <li className="FriendlyLinks-li">要申请友链的小伙伴们，请到博客<Link to="/blog/guestbook" className="FriendlyLinks-jump"><strong>留言版</strong></Link>进行留言，我将会在收到的第一时间进行添加，谢谢！</li>
+                        </ol>
                     </footer>
                 </div>
             </DocumentTitle>
