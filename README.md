a8482fb33744381873fb8c7d57c37f1084b73c5e
bs32g1038 <bs32g1038@163.com>
2018-12-13 04:09:22 -0800

    init

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-# react-blog-admin
+# node-blog-admin
 
 #### 项目介绍
-{**以下是码云平台说明，您可以替换为您的项目简介**
-码云是开源中国推出的基于 Git 的代码托管平台（同时支持 SVN）。专为开发者提供稳定、高效、安全的云端软件开发协作平台
-无论是个人、团队、或是企业，都能够用码云实现代码托管、项目管理、协作开发。企业项目请看 [https://gitee.com/enterprises](https://gitee.com/enterprises)}
+基于nodejs的博客管理后台界面
 
 #### 软件架构
 软件架构说明
@@ -35,5 +33,5 @@
 2. 码云官方博客 [blog.gitee.com](https://blog.gitee.com)
 3. 你可以 [https://gitee.com/explore](https://gitee.com/explore) 这个地址来了解码云上的优秀开源项目
 4. [GVP](https://gitee.com/gvp) 全称是码云最有价值开源项目，是码云综合评定出的优秀开源项目
-5. 码云官方提供的使用手册 [https://gitee.com/help](https://gitee.com/help)
+5. 码云官方提供的使用手册 [http://git.mydoc.io/](http://git.mydoc.io/)
 6. 码云封面人物是一档用来展示码云会员风采的栏目 [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
---
+++ build/webpack.prod.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const autoprefixer = {
+    loader: "postcss-loader",
+    options: {
+        plugins: function () {
+            return [
+                require('autoprefixer')
+            ];
+        }
+    }
+};
+module.exports = merge(common, {
+    mode: 'production',
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: '/static/admin/',
+        filename: '[name].bundle.js'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                include: [/cssmodule/],
+                use: ['style-loader', {
+                    loader: 'css-loader',
+                    options: {
+                        modules: true,
+                        localIdentName: '[local]-[hash:base64:5]'
+                    }
+                }, 'sass-loader']
+            }, {
+                test: /\.scss$/,
+                exclude: [/cssmodule/],
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, {
+                    loader: 'css-loader',
+                    options: {
+                        minimize: true
+                    }
+                }, autoprefixer]
+            }
+        ]
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJSPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true // set to true if you want JS source maps
+            }),
+            new OptimizeCssAssetsPlugin({})
+        ],
+        splitChunks: {
+            cacheGroups: {
+                styles: {
+                    name: 'styles',
+                    test: module => module.nameForCondition
+                        && /\.(css|scss)$/.test(module.nameForCondition())
+                        && !/^javascript/.test(module.type),
+                    chunks: 'all',
+                    enforce: true
+                }
+            }
+        }
+    }
+});
