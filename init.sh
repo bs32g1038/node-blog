9a6a0fb3e5ce0d8b35dfa3c4b3348ce7077dd82b
bs32g1038 <bs32g1038@163.com>
2019-01-19 15:53:29 -0800

    update

---
+++ init.sh
@@ -0,0 +1,7 @@
+git clone https://gitee.com/chengli01/node-blog.git ../node-blog
+
+git clone https://gitee.com/chengli01/react-blog.git ../react-blog
+
+git clone https://gitee.com/chengli01/react-blog-admin.git ../react-blog-admin
+
+git clone https://gitee.com/chengli01/blog-nginx.git ../blog-nginx
