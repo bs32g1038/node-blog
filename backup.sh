1e6ab42508e5113e630a8aa5c42b53662c96ef52
bs32g1038 <bs32g1038@163.com>
2019-01-19 19:43:06 -0800

    u

--- backup.sh
+++ backup.sh
@@ -5,9 +5,9 @@ DATE=`date +%Y_%m_%d`
 #DAYS=120代表删除120天前的备份，即只保留近120天的备份
 DAYS=120
 #备份全部数据库
-docker-compose exec db sh -c 'exec mongodump -d <blog> --archive' > /data/mongodb-backup
+docker-compose exec db sh -c 'exec mongodump -d <blog> --archive' > /data/mongo-backup
 #压缩为.tar.gz格式
-tar -zcvf /www/data/mongodb-backup/$DATE.tar.gz /www/data/mongodb-backup
+tar -zcvf /www/data/mongo-backup/$DATE.tar.gz /www/data/mongo-backup
 #删除120天前的备份文件
-find /www/data/mongodb-backup -mtime +$DAYS -delete
+find /www/data/mongo-backup -mtime +$DAYS -delete
 exit
