ef8497ab7ec3214ee76f4d258e52c84265bed924
bs32g1038 <bs32g1038@163.com>
2019-02-17 02:25:01 -0800

    update: init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
-git clone https://gitee.com/chengli01/node-blog.git ../node-blog
+git clone https://github.com/bs32g1038/node-blog-api.git ../node-blog-api
 
-git clone https://gitee.com/chengli01/react-blog.git ../react-blog
+git clone https://github.com/bs32g1038/react-blog.git ../react-blog
 
-git clone https://gitee.com/chengli01/react-blog-admin.git ../react-blog-admin
+git clone https://github.com/bs32g1038/react-blog-admin.git ../react-blog-admin
 
-git clone https://gitee.com/chengli01/blog-nginx.git ../blog-nginx
+git clone https://github.com/bs32g1038/blog-nginx.git ../blog-nginx
