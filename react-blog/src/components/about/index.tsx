23d71b1724cb5a606eb6fe0305d73e97ca7f0a09
bs32g1038 <bs32g1038@163.com>
2019-06-17 02:56:22 -0700

    chore: update about page css

--- react-blog/src/components/about/index.tsx
+++ react-blog/src/components/about/index.tsx
@@ -81,6 +81,49 @@ const AboutDiv = styled.div`
 
     .skills-list {
         display: flex;
+        margin-top: 20px;
+    }
+
+    .skillbar {
+        position: relative;
+        display: block;
+        background: #eee;
+        height: 30px;
+        max-width: 400px;
+        margin: 0 auto;
+        border-radius: 35px;
+        transition: 0.4s linear;
+        transition-property: width,background-color;
+    }
+
+    .skillbar-title {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 110px;
+        font-size: 14px;
+        color: #ffffff;
+        border-radius: 35px;
+    }
+
+    .skillbar-title span {
+        display: block;
+        background: rgba(0, 0, 0, 0.15);
+        padding: 0 20px;
+        height: 30px;
+        line-height: 30px;
+        border-radius: 35px;
+    }
+
+    .skill-bar-percent {
+        position: absolute;
+        right: 10px;
+        top: 0;
+        font-size: 12px;
+        height: 30px;
+        line-height: 30px;
+        color: #ffffff;
+        color: rgba(0, 0, 0, 0.5);
     }
 
     .skills-list ul {
@@ -91,64 +134,8 @@ const AboutDiv = styled.div`
         width: 50%;
     }
 
-    .skills-list ul li .progress {
-        position: relative;
-        display: block;
-        width: 100%;
-        height: 4px;
-        background: #eee;
-    }
-
-    .skills-list ul li .progress .percentage {
-        position: absolute;
-        left: 0;
-        top: 0;
-        width: 0%;
-        height: 100%;
-        background: #2eca7f;
-        transition: all 0.6s ease 0s;
-        border-radius: 4px;
-        &.html{
-            background-color: #e892bb;
-        }
-        &.css{
-            background-color: #ac97e0;
-        }
-        &.javascript{
-            background-color: #e0b176;
-        }
-        &.nodejs{
-            background-color: #4cf3ff;
-        }
-        &.mongodb{
-            background-color: #709441;
-        }
-        &.react{
-            background-color: #7fc8f8;
-        }
-        &.vue{
-            background-color: #51b1ef;
-        }
-    }
-
-    .skills-list ul li .progress {
-        position: relative;
-        display: block;
-        width: 100%;
-        height: 8px;
-        background: #eee;
-        border-radius: 4px;
-    }
-
-    .skills-list ul li {
-        position: relative;
-        padding: 15px 10px 0;
-    }
-
-    .skills-list ul li .name {
-        margin: 0 0 11px 0;
-        line-height: 16px;
-        font-weight: 400;
+    .skills-list li {
+        padding: 15px 0;
     }
 `;
 
@@ -186,9 +173,11 @@ class About extends React.Component<any, any> {
         const userProfile = this.props._DB.profile;
         let values = [];
         let userRepos;
+        let userInfo;
         let totalContributionLastYear;
         if (userProfile) {
             userRepos = userProfile.userRepos;
+            userInfo = userProfile.userInfo;
             totalContributionLastYear = userProfile.userCommits && userProfile.userCommits.total || 0;
             if (userProfile && userProfile.userCommits && userProfile.userCommits.contribution) {
                 values = userProfile.userCommits.contribution.map((item: any) => {
@@ -261,15 +250,15 @@ class About extends React.Component<any, any> {
                         <>
                             <Helmet title={siteInfo.name + '-关于'}></Helmet>
                             <h1 className="resume">--关于--</h1>
-                            <p style={{ padding: '10px 18px', margin: '0 10px', backgroundColor: '#efefef', borderRadius: '4px' }}>
+                            <p style={{ padding: '10px 18px', backgroundColor: '#efefef', borderRadius: '4px' }}>
                                 专注于web前端开发。喜欢新事物，关注前端动态，对新的技术有追求；涉猎广泛，喜欢 coding。
                             </p>
-                            <p style={{ padding: '0 10px' }}>
+                            <p>
                                 本博客是一个技术性博客，平时主要发布一些关于web前端以及后端开发的文章，这其中包括第三方的文章，也包括我自己总结的一些文章。 我会一直坚持写作，把自己的一些笔记记录下来，以作备忘和分享。
                             </p>
                             <div className="about-header">
                                 <div className="person-base">
-                                    <h2 className="name">冷夜流星</h2>
+                                    <h2 className="name">{userInfo.name}</h2>
                                     <h3 className="aim">求职目标：web前端工程师</h3>
                                 </div>
                                 <ul className="person-info">
@@ -278,12 +267,12 @@ class About extends React.Component<any, any> {
                                     <li><i className="fa fa-flag fa-fw"></i>计算机科学与技术</li>
                                 </ul>
                                 <ul className="person-info">
-                                    <li><i className="fa fa-map-marker fa-fw"></i>广东省广州市</li>
+                                    <li><i className="fa fa-map-marker fa-fw"></i>{userInfo.location}</li>
                                     <li><i className="fa fa-envelope fa-fw"></i>bs32g1038@163.com</li>
                                     <li><i className="fa fa-university fa-fw"></i>大学本科</li>
                                 </ul>
                                 <div className="person-img">
-                                    <img src="/public/images/avatar.png" alt="头像" />
+                                    <img src={userInfo.avatarUrl} alt="头像" />
                                     <h3 className="aim">web前端工程师</h3>
                                 </div>
                             </div>
@@ -310,60 +299,59 @@ class About extends React.Component<any, any> {
                             />
                             <ReactTooltip></ReactTooltip>
                             <div className="pie-chart">
-                                <h3 style={{ minWidth: 140 }}>Stars per Repo(top 12)</h3>
+                                <h3 style={{ minWidth: 140 }}>Stars per Repo(top 5)</h3>
                                 <PieChart userRepos={userRepos}></PieChart>
                             </div>
                             <div className="skills-list">
                                 <ul>
                                     <li className="border-line-h">
-                                        <div className="name">HTML5</div>
-                                        <div className="progress">
-                                            <div className="percentage html" style={{ width: '80%' }} title="80%"></div>
+                                        <div className="skillbar">
+                                            <div className="skillbar-title" style={{ background: 'linear-gradient(to right, #FF0066 0%, #FF00CC 100%); width: 80%' }}>
+                                                <span>HTML5</span>
+                                            </div>
+                                            <div className="skill-bar-percent">80%</div>
                                         </div>
                                     </li>
                                     <li className="border-line-h">
-                                        <div className="name">CSS3</div>
-                                        <div className="progress">
-                                            <div className="percentage css" style={{ width: '85%' }} title="85%"></div>
+                                        <div className="skillbar">
+                                            <div className="skillbar-title" style={{ background: 'linear-gradient(to right, #08adc2 0%, #ff00cc 100%); width: 85%' }}>
+                                                <span>CSS3</span>
+                                            </div>
+                                            <div className="skill-bar-percent">85%</div>
                                         </div>
                                     </li>
                                     <li className="border-line-h">
-                                        <div className="name">Mongodb</div>
-                                        <div className="progress">
-                                            <div className="percentage mongodb" style={{ width: '75%' }} title="75%"></div>
-                                        </div>
-                                    </li>
-                                    <li className="border-line-h">
-                                        <div className="name">Vue/Vue-router/Vuex</div>
-                                        <div className="progress">
-                                            <div className="percentage vue" style={{ width: '80%' }} title="80%"></div>
+                                        <div className="skillbar">
+                                            <div className="skillbar-title" style={{ background: 'linear-gradient(to right, #7fdf83 0%, #95c097 100%); width: 70%' }}>
+                                                <span>MONGODB</span>
+                                            </div>
+                                            <div className="skill-bar-percent">70%</div>
                                         </div>
                                     </li>
                                 </ul>
                                 <ul>
                                     <li className="border-line-h">
-                                        <div className="name">Javascript</div>
-                                        <div className="progress">
-                                            <div className="percentage javascript" style={{ width: '85%' }} title="85%"></div>
-                                        </div>
-                                    </li>
-                                    <li className="border-line-h">
-                                        <div className="name">NODEJS</div>
-                                        <div className="progress">
-                                            <div className="percentage nodejs" style={{ width: '77%' }} title="77%"></div>
-                                        </div>
-                                    </li>
-                                    <li className="border-line-h">
-                                        <div className="name">Git</div>
-                                        <div className="progress">
-                                            <div className="percentage git" style={{ width: '70%' }} title="70%">
+                                        <div className="skillbar">
+                                            <div className="skillbar-title" style={{ background: 'linear-gradient(to right, #b2694e 0%, #5f2712 100%); width: 85%' }}>
+                                                <span>JAVASCRIPT</span>
                                             </div>
+                                            <div className="skill-bar-percent">85%</div>
                                         </div>
                                     </li>
                                     <li className="border-line-h">
-                                        <div className="name">React/React-router/Redux</div>
-                                        <div className="progress">
-                                            <div className="percentage react" style={{ width: '82%' }} title="82%"></div>
+                                        <div className="skillbar">
+                                            <div className="skillbar-title" style={{ background: 'linear-gradient(to right, #7a0fbe 0%, #7632a1 100%); width: 75%' }}>
+                                                <span>NODEJS</span>
+                                            </div>
+                                            <div className="skill-bar-percent">75%</div>
+                                        </div>
+                                    </li>
+                                    <li className="border-line-h">
+                                        <div className="skillbar">
+                                            <div className="skillbar-title" style={{ background: 'linear-gradient(to right, #dbc92c 0%, #918734 100%); width: 70%' }}>
+                                                <span>GIT</span>
+                                            </div>
+                                            <div className="skill-bar-percent">70%</div>
                                         </div>
                                     </li>
                                 </ul>
