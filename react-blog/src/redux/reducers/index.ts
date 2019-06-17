50bc723bd0879960e1852b3d2c01525b208c10b8
bs32g1038 <bs32g1038@163.com>
2019-06-17 02:58:10 -0700

    chore: remove useless files

--- react-blog/src/redux/reducers/index.ts
+++ react-blog/src/redux/reducers/index.ts
@@ -4,14 +4,10 @@ import article from './article';
 import articles from './articles';
 import categories from './categories';
 import global from './global';
-import guestbooks from './guestbooks';
-import links from './links';
 
 export default combineReducers({
     article,
     articles,
-    guestbooks,
-    links,
     categories,
     about,
     $G: global
