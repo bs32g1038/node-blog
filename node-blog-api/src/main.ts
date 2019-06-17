357f94834d78a56d9fb73bfe59d243bcb0603b06
bs32g1038 <bs32g1038@163.com>
2019-06-17 01:37:53 -0700

    feat: add static file map

--- node-blog-api/src/main.ts
+++ node-blog-api/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { resolve } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+    const app = await NestFactory.create<NestExpressApplication>(
+        AppModule
+    );
+    const root: any = resolve(__dirname, '../static');
+    app.useStaticAssets(root, {
+        prefix: '/static/'
+    });
+    await app.listen(8080);
 }
 bootstrap();
