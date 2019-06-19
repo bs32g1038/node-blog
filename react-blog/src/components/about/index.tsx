34293aa7d18d97181b02c65a7fa7df6da4de0559
bs32g1038 <bs32g1038@163.com>
2019-06-19 01:22:06 -0700

    chore: adjust css

--- react-blog/src/components/about/index.tsx
+++ react-blog/src/components/about/index.tsx
@@ -84,17 +84,31 @@ const AboutDiv = styled.div`
         margin-top: 20px;
     }
 
-    .skillbar {
-        position: relative;
-        display: block;
-        background: #eee;
-        height: 30px;
-        max-width: 400px;
-        margin: 0 auto;
-        border-radius: 35px;
-        transition: 0.4s linear;
-        transition-property: width,background-color;
+    .skills-list ul {
+        list-style: none;
+        padding-left: 0;
+        margin: 0;
+        flex: 1 0 auto;
+        width: 50%;
+        margin-right: 20px;
+        &:last-child {
+            margin-right: -20px;
+        }
     }
+
+    .skills-list li {
+        padding: 15px 0;
+    }
+`;
+
+const Skillbar = styled.div`
+    position: relative;
+    display: block;
+    background: #eee;
+    height: 30px;
+    border-radius: 35px;
+    transition: 0.4s linear;
+    transition-property: width,background-color;
 
     .skillbar-title {
         position: absolute;
@@ -104,6 +118,30 @@ const AboutDiv = styled.div`
         font-size: 14px;
         color: #ffffff;
         border-radius: 35px;
+        &.html5 {
+            background: linear-gradient(to right, #ff0066 0%, #9b6d92 100%);
+            width: 80%;
+        }
+        &.css3 {
+            background: linear-gradient(to right, #08adc2 0%, #ff00cc 100%);
+            width: 85%
+        }
+        &.mongodb {
+            background: linear-gradient(to right, #7fdf83 0%, #95c097 100%);
+            width: 70%
+        }
+        &.javascript {
+            background: linear-gradient(to right, #b2694e 0%, #c4aba2 100%);
+            width: 85%
+        }
+        &.nodejs {
+            background: linear-gradient(to right, #7a0fbe 0%, #7632a1 100%);
+            width: 75%
+        }
+        &.git{
+            background: linear-gradient(to right, #dbc92c 0%, #918734 100%); 
+            width: 70%;
+        }
     }
 
     .skillbar-title span {
@@ -124,18 +162,6 @@ const AboutDiv = styled.div`
         line-height: 30px;
         color: #ffffff;
         color: rgba(0, 0, 0, 0.5);
-    }
-
-    .skills-list ul {
-        list-style: none;
-        padding-left: 0;
-        margin: 0;
-        flex: 1 0 auto;
-        width: 50%;
-    }
-
-    .skills-list li {
-        padding: 15px 0;
     }
 `;
 
@@ -307,54 +333,54 @@ class About extends React.Component<any, any> {
                             <div className="skills-list">
                                 <ul>
                                     <li className="border-line-h">
-                                        <div className="skillbar">
-                                            <div className="skillbar-title" style={{ background: 'linear-gradient(to right, #FF0066 0%, #FF00CC 100%); width: 80%' }}>
+                                        <Skillbar>
+                                            <div className="skillbar-title html5">
                                                 <span>HTML5</span>
                                             </div>
                                             <div className="skill-bar-percent">80%</div>
-                                        </div>
+                                        </Skillbar>
                                     </li>
                                     <li className="border-line-h">
-                                        <div className="skillbar">
-                                            <div className="skillbar-title" style={{ background: 'linear-gradient(to right, #08adc2 0%, #ff00cc 100%); width: 85%' }}>
+                                        <Skillbar>
+                                            <div className="skillbar-title css3">
                                                 <span>CSS3</span>
                                             </div>
                                             <div className="skill-bar-percent">85%</div>
-                                        </div>
+                                        </Skillbar>
                                     </li>
                                     <li className="border-line-h">
-                                        <div className="skillbar">
-                                            <div className="skillbar-title" style={{ background: 'linear-gradient(to right, #7fdf83 0%, #95c097 100%); width: 70%' }}>
+                                        <Skillbar>
+                                            <div className="skillbar-title mongodb">
                                                 <span>MONGODB</span>
                                             </div>
                                             <div className="skill-bar-percent">70%</div>
-                                        </div>
+                                        </Skillbar>
                                     </li>
                                 </ul>
                                 <ul>
                                     <li className="border-line-h">
-                                        <div className="skillbar">
-                                            <div className="skillbar-title" style={{ background: 'linear-gradient(to right, #b2694e 0%, #5f2712 100%); width: 85%' }}>
+                                        <Skillbar>
+                                            <div className="skillbar-title javascript" >
                                                 <span>JAVASCRIPT</span>
                                             </div>
                                             <div className="skill-bar-percent">85%</div>
-                                        </div>
+                                        </Skillbar>
                                     </li>
                                     <li className="border-line-h">
-                                        <div className="skillbar">
-                                            <div className="skillbar-title" style={{ background: 'linear-gradient(to right, #7a0fbe 0%, #7632a1 100%); width: 75%' }}>
+                                        <Skillbar>
+                                            <div className="skillbar-title nodejs">
                                                 <span>NODEJS</span>
                                             </div>
                                             <div className="skill-bar-percent">75%</div>
-                                        </div>
+                                        </Skillbar>
                                     </li>
                                     <li className="border-line-h">
-                                        <div className="skillbar">
-                                            <div className="skillbar-title" style={{ background: 'linear-gradient(to right, #dbc92c 0%, #918734 100%); width: 70%' }}>
+                                        <Skillbar>
+                                            <div className="skillbar-title git" >
                                                 <span>GIT</span>
                                             </div>
                                             <div className="skill-bar-percent">70%</div>
-                                        </div>
+                                        </Skillbar>
                                     </li>
                                 </ul>
                             </div>
