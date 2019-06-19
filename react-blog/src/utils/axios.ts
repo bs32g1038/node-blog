73d892b8896e99d487fd0225e3b98f7210ad7a0c
bs32g1038 <bs32g1038@163.com>
2019-06-19 08:12:25 -0700

    chore: increase timeout to 5000

--- react-blog/src/utils/axios.ts
+++ react-blog/src/utils/axios.ts
@@ -12,5 +12,5 @@ if (typeof window !== 'undefined') {
 const instance = axios.create({
     baseURL: baseUrl
 });
-instance.defaults.timeout = 3000;
+instance.defaults.timeout = 5000;
 export default instance;
