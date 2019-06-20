1401029da2364b52c96f57786a5bf566e4460c88
bs32g1038 <bs32g1038@163.com>
2019-06-19 17:33:58 -0700

    chore: realize the typesciprt support in razzle.config.js

--- react-blog/razzle.config.js
+++ react-blog/razzle.config.js
@@ -1,9 +1,37 @@
 'use strict';
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const path = require('path');
+
+const isDev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    plugins: ['typescript'],
     modify: (config, { target, dev }, webpack) => {
         const appConfig = config;
+        appConfig.resolve.extensions = [...appConfig.resolve.extensions, '.ts', '.tsx'];
+        appConfig.module.rules.push({
+            test: /\.(ts|tsx)?$/,
+            loader: 'ts-loader',
+            options: {
+                // disable type checker - we will use it in fork plugin
+                transpileOnly: true
+            }
+        });
+        appConfig.plugins.push(new ForkTsCheckerWebpackPlugin({
+            tsconfig: path.resolve(__dirname, './tsconfig.json'),
+            tslint: isDev ? path.resolve(__dirname, './tslint.json') : '',
+            async: isDev,
+            useTypescriptIncrementalApi: true,
+            checkSyntacticErrors: true,
+            reportFiles: [
+                '**',
+                '!**/__tests__/**',
+                '!**/?(*.)(spec|test).*',
+                '!**/src/setupProxy.*',
+                '!**/src/setupTests.*',
+            ],
+            watch: [path.resolve(__dirname, './src')],
+            silent: true
+        }))
         return appConfig;
     },
 };
