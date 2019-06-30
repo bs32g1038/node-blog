1ca6528c751fe8297d66d82da0c19fa3d7cc844a
bs32g1038 <bs32g1038@163.com>
2019-06-29 22:50:19 -0700

    chore: release docker

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,23 +1,7 @@
-dockerfile="
-FROM node:10.16.0-alpine\n
-WORKDIR /code\n
-COPY ./package.json /code\n
-RUN yarn install --production\n
-COPY . /code\n
-"
-echo -e $dockerfile"\nEXPOSE 8080" > node-blog-api/dockerfile
-echo -e $dockerfile"\nEXPOSE 3002" > react-admin/dockerfile
-echo -e $dockerfile"\nEXPOSE 3000" > react-blog/dockerfile
-echo -e $dockerfile"\nEXPOSE 7000" > react-music-app/dockerfile
-echo "
-.cache
-node_modules
-.vscode
-logs/blog/*
-!.gitkeep
-*.lock
-*.log
-" > .gitignore
-git add --all .
-git commit --message "Automatically update from travis-ci"
-git push --force "https://${GH_TOKEN}@${GH_REF}" HEAD:deploy
+set -e
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+docker-compose -f docker-compose.release.yml build
+
+docker-compose -f docker-compose.release.yml push
