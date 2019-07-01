7c2e12ce886924968f9c64b8bd3a9fa3090499bc
bs32g1038 <bs32g1038@163.com>
2019-07-01 07:05:24 -0700

    chore: add clone-project shell

---
+++ scripts/clone-project.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/lc-notes/blog-web.git ../blog-web
+
+git clone https://github.com/lc-notes/blog-admin.git ../blog-admin
+
+git clone https://github.com/lc-notes/blog-server.git ../blog-server
+
+git clone https://github.com/lc-notes/blog-music-app.git ../blog-music-app
+
+git clone https://github.com/lc-notes/blog-nginx.git ../blog-nginx
