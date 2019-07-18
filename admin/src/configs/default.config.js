6b38169af2a4bad959605fc5be706b15146d4c54
bs32g1038 <bs32g1038@163.com>
2019-07-17 18:36:38 -0700

    chore: adjust config info

--- admin/src/configs/default.config.js
+++ admin/src/configs/default.config.js
@@ -1,8 +1,8 @@
 export default {
-    title: '博客后台系统',
+    title: '网站后台系统',
     tokenKey: 'node-blog-bs32g1038@163.com',
     siteInfo: {
-        name: 'Lizc博客',
+        name: '李志成的博客',
         icp: '粤ICP备16021965号-2',
         github: 'https://github.com/bs32g1038/node-blog',
         domain: 'http://www.lizc.net',
