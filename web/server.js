625ac10ab67ca778cd1689c8ab4a23fd67b6dd74
bs32g1038 <bs32g1038@163.com>
2019-07-18 04:36:51 -0700

    chore: better handling of "next relative files"

--- web/server.js
+++ web/server.js
@@ -1,7 +1,6 @@
 /* eslint-disable */
 const express = require('express');
 const next = require('next');
-const { parse } = require('url');
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -11,25 +10,26 @@ let proxy = require('http-proxy-middleware');
 
 app.prepare().then(() => {
     const server = express();
+    server.use(helmet());
+    server.get('/_next/*', (req, res) => {
+        handle(req, res);
+    });
     if (dev) {
         server.use('/api', proxy({ target: 'http://127.0.0.1:8080', changeOrigin: true }));
+        server.get('/static/upload/*', proxy({ target: 'http://127.0.0.1:8080', changeOrigin: true }));
     } else {
         server.use('/api', proxy({ target: 'http://www.lizc.net', changeOrigin: true }));
     }
-    server.get('/blog', (req, res) => {
-        return app.render(req, res, '/articles', { cid: req.params.id });
+    server.get('/static/*', (req, res) => {
+        handle(req, res);
     });
-    server.get('/blog/categories/:id', (req, res) => {
-        return app.render(req, res, '/articles', { cid: req.params.id });
-    });
+
     server.get('/blog/articles/:id', (req, res) => {
-        return app.render(req, res, '/article', { id: req.params.id });
+        return app.render(req, res, '/blog/article', { id: req.params.id });
     });
     server.get('/', (req, res) => res.redirect('/blog'));
-    server.use(helmet());
-    server.use((req, res) => {
-        const parsedUrl = parse(req.url, true);
-        handle(req, res, parsedUrl);
+    server.get('*', (req, res) => {
+        handle(req, res);
     });
     server.listen(3000, err => {
         if (err) throw err;
