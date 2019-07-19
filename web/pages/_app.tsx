3b99115893aa98bb061ff4949c90a5ec77b36673
bs32g1038 <bs32g1038@163.com>
2019-07-18 21:17:26 -0700

    fix: version info console

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -2,8 +2,23 @@ import App, { Container } from 'next/app';
 import React from 'react';
 import { Provider } from 'react-redux';
 import withReduxStore from '../redux/with-redux-store';
+import versionInfo from '../../package.json';
 
 class MyApp extends App {
+    componentDidMount() {
+        const info = [
+            `Version: ${versionInfo.version}`,
+            `Author: ${versionInfo.author.name + ' - ' + versionInfo.author.email}`,
+            `Homepage: ${versionInfo.homepage}`,
+            `Description: ${versionInfo.description}`,
+            `Check out our code here: https://github.com/bs32g1038/node-blog`,
+            `Have a great day! 📣🐢`,
+        ];
+        for (const message of info) {
+            // eslint-disable-next-line no-console
+            console.log(message);
+        }
+    }
     public render() {
         const { Component, pageProps, reduxStore } = this.props as any;
         return (
