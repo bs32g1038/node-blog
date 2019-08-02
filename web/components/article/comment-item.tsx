321523ead7a1b284e440c0b009ce6e7522ee232d
bs32g1038 <bs32g1038@163.com>
2019-08-02 01:37:07 -0700

    refactor: better presentation of comments

--- web/components/article/comment-item.tsx
+++ web/components/article/comment-item.tsx
@@ -2,7 +2,8 @@ import styled from '@emotion/styled';
 import React, { useState, useEffect } from 'react';
 import { xss } from '../../utils/helper';
 import marked from '../../utils/marked';
-import { parseTime, timeAgo } from '../../utils/time';
+import { timeAgo } from '../../utils/time';
+import media from '../../utils/media';
 import GHAT from '../../utils/generate-avatar';
 import { CommentForm } from '../comment-form';
 import md5 from 'crypto-js/md5';
@@ -22,6 +23,11 @@ const CommentsItem = styled.li`
         color: #d5cbcb;
         font-size: 12px;
     }
+    ${media.phone`
+        &:after {
+            display: none;
+        }
+    `}
 `;
 
 const Info = styled.div`
@@ -40,6 +46,16 @@ const AvatarWrap = styled.div`
         vertical-align: middle;
         border-radius: 4px;
     }
+    &.quote {
+        display: inline-block;
+        width: 16px;
+        height: 16px;
+        margin-top: 0;
+        img {
+            width: 16px;
+            vertical-align: inherit;
+        }
+    }
 `;
 
 const Content = styled.div`
@@ -49,29 +65,58 @@ const Content = styled.div`
 const Meta = styled.div`
     color: #999;
     font-size: 12px;
+    margin-right: 60px;
     > a {
         text-decoration: none;
         color: #999;
+        margin-right: 12px;
     }
+    &.quote {
+        margin-right: 0;
+    }
+    ${media.phone`
+        margin-right: 0;
+        a {
+            margin-right: 5px;
+        }
+    `}
 `;
 
-const User = styled.div``;
+const User = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
 
 const NickName = styled.span`
     font-weight: 700;
     word-wrap: break-word;
     color: #6d757a;
+    display: flex;
+    align-items: center;
+    ${media.phone`
+        .tip {
+            display: none;
+        }
+    `}
+`;
+
+const InfoTime = styled.span`
+    color: #999;
+    font-size: 12px;
+    font-weight: normal;
 `;
 
 const UserSign: any = styled.span`
     margin-left: 5px;
-    color: #fff;
-    background-color: rgba(58, 165, 208, 0.8);
+    color: #999;
+    /* background-color: rgba(58, 165, 208, 0.8); */
     padding: 0 4px;
-    border-radius: 2px;
+    border-radius: 5px;
     font-size: 12px;
     display: inline-block;
     ${(props: any) => props.isAdmin && 'background-color: rgba(250, 90, 60, .95)'};
+    border-bottom: 1px solid #999;
 `;
 
 const ItemContent = styled.div`
@@ -90,7 +135,7 @@ const Quote = styled.div`
     background-color: #f5f5f5;
     font-size: 14px;
     margin-bottom: 10px;
-    padding: 5px 20px 5px 20px;
+    padding: 10px 20px 10px 20px;
     margin-top: 10px;
     border-radius: 5px;
     display: flex;
@@ -102,21 +147,38 @@ const ReplyBox = styled.div`
 
 const replyFn = (item: any) => {
     const [avatarSrc, setAvatarSrc] = useState('');
+    const [showContent, setShowContent] = useState(false);
     useEffect(() => {
         setAvatarSrc(ghat.getImage(md5(item.nickName).toString()) || '');
     }, item._id);
     return (
         <Quote>
-            <AvatarWrap>
-                <img src={avatarSrc} />
-            </AvatarWrap>
             <Content>
                 <User>
-                    <NickName>{item.nickName}</NickName>
-                    <UserSign isAdmin={item.identity !== 0}>{item.identity !== 0 ? '博主' : '游客'}</UserSign>
+                    <NickName>
+                        <span className="tip">回复给：</span>
+                        <AvatarWrap className="quote">
+                            <img src={avatarSrc} />
+                        </AvatarWrap>
+                        {item.nickName}
+                        <InfoTime>&nbsp;·&nbsp;{timeAgo(item.createdAt)}</InfoTime>
+                    </NickName>
+                    <Meta className="quote">
+                        <a
+                            href="javascript:;"
+                            comment-id={item._id}
+                            onClick={() => {
+                                setShowContent(!showContent);
+                            }}
+                        >
+                            {showContent ? '折叠' : '展开'}
+                        </a>
+                        <UserSign isAdmin={item.identity !== 0}>{item.identity !== 0 ? '博主' : '游客'}</UserSign>
+                    </Meta>
                 </User>
-                <ItemContent dangerouslySetInnerHTML={{ __html: xss(marked(item.content)) }}></ItemContent>
-                <Meta>发表于：{timeAgo(item.createdAt)}前</Meta>
+                {showContent && (
+                    <ItemContent dangerouslySetInnerHTML={{ __html: xss(marked(item.content)) }}></ItemContent>
+                )}
             </Content>
         </Quote>
     );
@@ -138,22 +200,24 @@ export const CommentItem = (props: { item: any; index: number }) => {
                 </AvatarWrap>
                 <Content>
                     <User>
-                        <NickName>{item.nickName}</NickName>
-                        <UserSign isAdmin={item.identity !== 0}>{item.identity !== 0 ? '博主' : '游客'}</UserSign>
+                        <NickName>
+                            {item.nickName}
+                            <InfoTime>&nbsp;·&nbsp;{timeAgo(item.createdAt)}</InfoTime>
+                        </NickName>
+                        <Meta>
+                            <a
+                                style={{ color: '#f86422' }}
+                                href="javascript:;"
+                                comment-id={item._id}
+                                onClick={() => setShowCommentForm(showCommentForm ? '' : item._id)}
+                            >
+                                回复
+                            </a>
+                            <UserSign isAdmin={item.identity !== 0}>{item.identity !== 0 ? '博主' : '游客'}</UserSign>
+                        </Meta>
                     </User>
-                    <ItemContent dangerouslySetInnerHTML={{ __html: xss(marked(item.content)) }}></ItemContent>
-                    <Meta>
-                        <span className="ArticleComments-infoTime">发表于：{parseTime(item.createdAt)}</span>
-                        <span> | </span>
-                        <a
-                            href="javascript:;"
-                            comment-id={item._id}
-                            onClick={() => setShowCommentForm(showCommentForm ? '' : item._id)}
-                        >
-                            回复
-                        </a>
-                    </Meta>
                     {item.reply && replyFn(item.reply)}
+                    <ItemContent dangerouslySetInnerHTML={{ __html: xss(marked(item.content)) }}></ItemContent>
                     <ReplyBox>
                         {showCommentForm === item._id && (
                             <CommentForm url="/comments" articleId={item.article} replyId={item._id} />
