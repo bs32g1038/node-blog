5a7429b8ad904c443759c51baccf438dcdc84841
bs32g1038 <bs32g1038@163.com>
2020-03-05 18:33:58 -0800

    chore: update test

--- test/e2e/user.module.e2e.spec.ts
+++ test/e2e/user.module.e2e.spec.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { UserModule } from '@blog/server/modules/user/user.module';
 import { INestApplication } from '@nestjs/common';
 import { initApp, closeApp } from '../util';
+import { encrypt } from '@blog/server/utils/crypto.util';
 
 /**
  * 用户模块 api 测试
@@ -20,6 +21,46 @@ describe('user.module.e2e', () => {
             .get('/api/user/login-info')
             .set('authorization', __TOKEN__)
             .expect(200);
+    });
+
+    test('forbidden request, get user login info failure', async () => {
+        return request(app.getHttpServer())
+            .get('/api/user/login-info')
+            .expect(403);
+    });
+
+    test('update user info success', async () => {
+        return request(app.getHttpServer())
+            .put('/api/user/update')
+            .set('authorization', __TOKEN__)
+            .send({
+                avatar: 'http://127.0.0.1/test.jpg',
+                userName: 'test',
+                email: 'test@test.com',
+            })
+            .expect(200);
+    });
+
+    test('forbidden request, update user info failure', async () => {
+        return request(app.getHttpServer())
+            .put('/api/user/update')
+            .expect(403);
+    });
+
+    test('reset-password success', async () => {
+        return request(app.getHttpServer())
+            .put('/api/user/reset-password')
+            .set('authorization', __TOKEN__)
+            .send({
+                password: encrypt('password'),
+            })
+            .expect(200);
+    });
+
+    test('forbidden request, reset-password failure', async () => {
+        return request(app.getHttpServer())
+            .put('/api/user/reset-password')
+            .expect(403);
     });
 
     afterAll(async () => {
