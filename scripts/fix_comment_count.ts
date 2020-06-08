9e2612ed68d6c73bcc10200e4f0cc46fa175cfdc
bs32g1038 <bs32g1038@163.com>
2020-06-08 10:25:09 +0800

    chore: add fix comment count script

---
+++ scripts/fix_comment_count.ts
@@ -0,0 +1,28 @@
+import { MONGODB } from '../server/configs/index.config';
+import mongoose from 'mongoose';
+import { ArticleModel, CommentModel } from '../test/models';
+
+const init = async () => {
+    const conn = await mongoose.connect(MONGODB.uri, {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    });
+
+    const articles = await ArticleModel.find({});
+    Promise.all(
+        articles.map(async (item) => {
+            const count = await CommentModel.countDocuments({ article: item._id });
+            await this.articleModel.updateOne({ _id: item._id }, { commentCount: count });
+        })
+    );
+
+    console.log('修复数据完成。。。');
+
+    conn.connection.close();
+
+    process.exit(0);
+};
+
+init();
