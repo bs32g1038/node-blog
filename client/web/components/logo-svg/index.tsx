e242d73d4d973101e3beb23f9174f9f595d58faf
bs32g1038 <bs32g1038@163.com>
2023-01-19 10:32:41 +0800

    fix: server render logo request error

--- client/web/components/logo-svg/index.tsx
+++ client/web/components/logo-svg/index.tsx
@@ -3,6 +3,6 @@ import { useFetchConfigQuery, useFetchConfigSvgQuery } from '../../api';
 
 export default function LogoSvg(props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>) {
     const { data: config } = useFetchConfigQuery();
-    const { data } = useFetchConfigSvgQuery({ url: config.siteLogo });
+    const { data } = useFetchConfigSvgQuery({ url: config.siteLogo.replace('//' + config.siteDomain, '') });
     return <div dangerouslySetInnerHTML={{ __html: data }} {...props}></div>;
 }
