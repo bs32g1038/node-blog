30fe251bc3bf556958f9a91161f5603b1ff72a44
bs32g1038 <bs32g1038@163.com>
2023-02-28 14:26:15 +0800

    fix: logo ssr load failure

--- client/web/components/logo-svg/index.tsx
+++ client/web/components/logo-svg/index.tsx
@@ -3,6 +3,12 @@ import { useFetchConfigQuery, useFetchConfigSvgQuery } from '../../api';
 
 export default function LogoSvg(props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>) {
     const { data: config } = useFetchConfigQuery();
-    const { data } = useFetchConfigSvgQuery({ url: config.siteLogo.replace('//' + config.siteDomain, '') });
+    let url = config.siteLogo;
+    if (config.siteLogo.indexOf(config.siteDomain) > 0) {
+        url = config.siteLogo.substring(config.siteLogo.indexOf(config.siteDomain) + config.siteDomain.length);
+    }
+    const { data } = useFetchConfigSvgQuery({
+        url,
+    });
     return <div dangerouslySetInnerHTML={{ __html: data }} {...props}></div>;
 }
